713ed3d2303a24dfde2cf0a5f5f5a74e815cccf1
S.Adhikary <sadhikary0055@gmail.com>
2025-09-30 19:49:56 +0530

    order service

--- inventory-service/src/main/java/com/rapidreserve/inventory_service/controller/InventoryController.java
+++ inventory-service/src/main/java/com/rapidreserve/inventory_service/controller/InventoryController.java
@@ -5,6 +5,7 @@ import com.rapidreserve.inventory_service.response.EventInventoryResponse;
 import com.rapidreserve.inventory_service.response.VenueInventoryResponse;
 import com.rapidreserve.inventory_service.service.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -37,5 +38,13 @@ public class InventoryController {
         EventInventoryResponse event = inventoryService.getEventInventory(eventId);
         return ApiResponse.success(event, "Events retrieved successfully");
     }
+
+    @PutMapping("/inventory/event/{eventId}/capacity/{capacity}")
+    public @ResponseBody ApiResponse<Void> updateEventCapacity(@PathVariable("eventId") Long eventId,
+                                                               @PathVariable("capacity") Long ticketsBooked) {
+        inventoryService.updateEventCapacity(eventId, ticketsBooked);
+        return ApiResponse.success(null, "Event capacity updated successfully");
+    }
+
 
 }
